5563200efbc1cec1276a62a4a91fe7575d46e419
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-09-04 22:41:24 +0100

    alx_airbnb join

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,12 @@
+use `alx_airbnb_database`;
+
+
+SELECT * from Booking join User on Booking.user_id = User.id  LIMIT 3;
+
+SELECT * from Property LEFT JOIN Review on Property.property_id = Review.property_id LIMIT 10 ;
+
+
+
+select * from User  RIGHT JOIN Booking on User.id = Booking.user_id 
+UNION
+select * from User  LEFT JOIN Booking on User.id = Booking.user_id  LIMIT 10;
--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -21,9 +21,9 @@ DROP TABLE IF EXISTS `Message`;
 -- -------------------------
 CREATE TABLE `User` (
   `id` CHAR(36) NOT NULL DEFAULT (UUID()),
-  `first_name` VARCHAR(25) NOT NULL,
-  `last_name` VARCHAR(25) NOT NULL,
-  `email` VARCHAR(25) NOT NULL,
+  `first_name` VARCHAR(50) NOT NULL,
+  `last_name` VARCHAR(50) NOT NULL,
+  `email` VARCHAR(50) NOT NULL,
   `password_hash` VARCHAR(255) NOT NULL,
   `phone_number` VARCHAR(25) DEFAULT NULL,
   `role` ENUM('guest','host','admin') NOT NULL,
