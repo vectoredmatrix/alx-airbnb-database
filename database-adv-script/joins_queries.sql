dbc56bd1d9c1f0234373265a718dd01f35188035
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-09-04 22:55:24 +0100

    alx_airbnb join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,11 +1,11 @@
 --use `alx_airbnb_database`;
 
-SELECT * from Booking INNER join User on Booking.user_id = User.id  LIMIT 3;
+SELECT * FROM Booking INNER JOIN User on Booking.user_id = User.id  LIMIT 3;
 
-SELECT * from Property LEFT JOIN Review on Property.property_id = Review.property_id LIMIT 10 ;
+SELECT * FROM Property LEFT JOIN Review on Property.property_id = Review.property_id LIMIT 10 ;
 
 
 
-select * from User  RIGHT JOIN Booking on User.id = Booking.user_id 
+select * FROM User  RIGHT JOIN Booking on User.id = Booking.user_id 
 UNION
-select * from User  LEFT JOIN Booking on User.id = Booking.user_id  LIMIT 10;
+select * FROM User  LEFT JOIN Booking on User.id = Booking.user_id  LIMIT 10;
