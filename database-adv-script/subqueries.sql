533f5f98102834985cd9f1e7e919fb16ba5b614c
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-09-05 00:27:33 +0100

    alx_airbnb join

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -2,7 +2,11 @@ use `alx_airbnb_database`;
 
 SELECT  *
 FROM Property
-WHERE Property.property_id IN ( SELECT Review.property_id FROM Review WHERE Review.rating > 4 ); use `alx_airbnb_database`;
+WHERE Property.property_id IN 
+( SELECT Review.property_id 
+FROM Review 
+WHERE Review.rating > 4.0 ); 
+
 
 SELECT  user_id
        ,COUNT(*) AS TOTAL_BOOKING
