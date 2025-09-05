876ed80f42f7272a5e12f8f3723626e2ed1c9c24
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-09-05 06:41:02 +0100

    alx_airbnb join

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,25 @@
+USE `alx_airbnb_database`
+
+SELECT 
+    p.property_id,
+    p.name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank
+FROM Property p
+LEFT JOIN Booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name
+ORDER BY rank;
+
+
+
+SELECT 
+    p.property_id,
+    p.name,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank
+FROM Property p
+LEFT JOIN Booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name
+ORDER BY rank;
+
+
